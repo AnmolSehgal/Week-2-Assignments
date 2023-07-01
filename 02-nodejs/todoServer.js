b9ed249b94c3417191fd3174878ce495950f6e0a
Anmol Sehgal <anmolsehgal@Anmols-MacBook-Air.local>
2023-07-01 15:20:14 +0530

    feature: creating todo app

--- 02-nodejs/todoServer.js
+++ 02-nodejs/todoServer.js
@@ -39,11 +39,157 @@
 
   Testing the server - run `npm run test-todoServer` command in terminal
  */
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const { v4: uuidV4 } = require("uuid");
+const fs = require("fs");
+const path = require("path");
+const cors = require("cors");
+
+const filename = path.join(__dirname, "./todo.json");
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
+
+app.get("/todos", (req, res) => {
+  if (fs.existsSync(filename)) {
+    fs.readFile(filename, "utf8", (err, data) => {
+      if (err) {
+        res.status(404).send("File not found");
+      } else {
+        res.status(200).json(JSON.parse(data));
+      }
+    });
+  } else {
+    fs.writeFile(filename, JSON.stringify([]), (err, data) => {
+      if (err) {
+        res.status(404).send("Backend issue");
+      } else {
+        res.status(200).json(JSON.parse(data));
+      }
+    });
+  }
+});
+
+app.get("/todos/:id", (req, res) => {
+  if (fs.existsSync(filename)) {
+    fs.readFile(filename, "utf8", (err, data) => {
+      if (err) {
+        res.status(404).send("File not found");
+      }
+      const todos = JSON.parse(data);
+
+      const index = todos.findIndex((item) => item.id === req.params.id);
+      if (index === -1) res.status(404).send("Not Found");
+      res.status(200).json(todos[index]);
+    });
+  } else {
+    res.status(404).send("Task not found");
+  }
+});
+
+app.post("/todos", (req, res) => {
+  const newTask = {
+    id: uuidV4(),
+    title: req.body.title,
+    description: req.body.description,
+    completed: false,
+  };
+
+  if (fs.existsSync(filename)) {
+    fs.readFile(filename, "utf8", (err, data) => {
+      if (err) {
+        res.status(404).send("File not found");
+      }
+      const todos = JSON.parse(data);
+      todos.push(newTask);
+
+      fs.writeFile(filename, JSON.stringify(todos), (err) => {
+        if (err) {
+          res.status(404).send("Backend issue");
+        } else res.status(201).json(newTask);
+      });
+    });
+  } else {
+    fs.writeFile(filename, JSON.stringify([newTask]), (err) => {
+      if (err) {
+        res.status(404).send("Backend issue");
+      } else res.status(201).json(newTask);
+    });
+  }
+});
+
+app.put("/todos/:id", (req, res) => {
+  if (fs.existsSync(filename)) {
+    fs.readFile(filename, "utf8", (err, data) => {
+      if (err) {
+        res.status(404).send("File not found");
+      }
+
+      const todos = JSON.parse(data);
+      const index = todos.findIndex((item) => item.id === req.params.id);
+
+      if (index === -1) res.status(404).send();
+
+      fs.writeFile(
+        filename,
+        JSON.stringify(
+          todos.map((todo) => ({
+            id: todo.id,
+            title: req.body.title || todo.title,
+            description: req.body.description || todo,
+            completed: req.body.completed || todo.completed,
+          }))
+        ),
+        (err, data) => {
+          if (err) {
+            res.status(404).send("Backend issue");
+          } else {
+            res.status(200).send();
+          }
+        }
+      );
+    });
+  } else {
+    fs.writeFile(filename, JSON.stringify([newTask]), (err, data) => {
+      if (err) {
+        res.status(404).send("Backend issue");
+      } else {
+        res.status(200).send();
+      }
+    });
+  }
+});
+
+app.delete("/todos/:id", (req, res) => {
+  if (fs.existsSync(filename)) {
+    fs.readFile(filename, "utf8", (err, data) => {
+      if (err) res.status(500).send();
+      console.log(req.params.id);
+      const todos = JSON.parse(data);
+      const index = todos.findIndex((item) => item.id === req.params.id);
+      if (index === -1) res.status(404).send();
+      else {
+        fs.writeFile(
+          filename,
+          JSON.stringify(todos.filter((_, i) => index !== i)),
+          (err) => {
+            if (err) {
+              res.status(404).send("Backend issue");
+            } else {
+              res.status(200).send();
+            }
+          }
+        );
+      }
+    });
+  } else res.status(404).send();
+});
+
+app.use((req, res) => {
+  res.status(404).send();
+});
 
 module.exports = app;
